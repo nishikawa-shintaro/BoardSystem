0247417bf24d56ab902119227706871cd18858b4
nishikawa-shintaro <nishikawa.shintaro@m.alhinc.jp>
2017-07-18 17:47:45 +0900

    更新

---
+++ src/boardsystem/filter/AccessFilter.java
@@ -0,0 +1,74 @@
+package boardsystem.filter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import boardsystem.beans.User;
+import boardsystem.service.UserService;
+
+
+@WebFilter({"/usercontrol","/useredit","/signup"})
+public class AccessFilter implements Filter {
+
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		//対象のサーブレットパスを文字列として取得
+		String url = ((HttpServletRequest) request).getServletPath();
+		HttpSession session = ((HttpServletRequest) request).getSession();
+
+		List<String> messages = new ArrayList<String>();
+
+		//ログインユーザーを取得
+		User user = (User)session.getAttribute("loginUser");
+
+		if(user== null){
+			messages.add("ログインしてください");
+			session.setAttribute("errorMessages", messages);
+			((HttpServletResponse) response).sendRedirect("login");
+			return;
+		}else{
+
+			User accessCheck = new UserService().getUser(user.getId());
+				//本部社員かつ人事総務部以外でエラーメッセージを表示
+			if(!(accessCheck.getBranchId()==1 && accessCheck.getPossitionId()==1)){
+
+				messages.add("アクセス権限がありません");
+				session.setAttribute("errorMessages",messages);
+				((HttpServletResponse) response).sendRedirect("./");
+
+				return;
+			}
+
+		}
+		chain.doFilter(request, response);
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void destroy() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/boardsystem/filter/LoginFilter.java
@@ -0,0 +1,76 @@
+package boardsystem.filter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import boardsystem.beans.User;
+import boardsystem.service.UserService;
+
+
+@WebFilter("/*")
+public class LoginFilter implements Filter {
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException{
+
+		//対象のサーブレットパスを文字列として取得
+		String url = ((HttpServletRequest) request).getServletPath();
+		HttpSession session = ((HttpServletRequest)request).getSession();
+		User user = (User)session.getAttribute("loginUser");
+
+		List<String> messages = new ArrayList<String>();
+
+
+		if(url.equals("/login") || url.equals("/login.jsp") ){
+			chain.doFilter(request, response);   //条件と一致した場合サーブレッドの実行
+			//System.out.println(user);
+		}else{
+			if(user == null){
+				//System.out.println(user);
+				messages.add("ログインしてください");
+				session.setAttribute("errorMessages", messages);
+				((HttpServletResponse) response).sendRedirect("login");
+
+				return;
+			}else{
+				//アカウント停止チェック]
+				User loginCheck = new UserService().getUser(user.getId());
+				if(loginCheck.getIsStopped()==0){
+					chain.doFilter(request, response);
+				}else{
+
+					messages.add("現在このアカウントは使用できません");
+					session.setAttribute("errorMessages", messages);
+					((HttpServletResponse) response).sendRedirect("login");
+
+				return;
+				}
+
+			}
+		}
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+
+	}
+
+}
+
